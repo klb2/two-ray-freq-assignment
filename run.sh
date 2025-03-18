01a4accdbb1d964937f45ab446efe166bd9c2a24
Karl-Ludwig Besser <karl-ludwig.besser@liu.se>
2025-03-18 10:25:49 +0100

    Update run script to reproduce all the results shown in the paper

--- run.sh
+++ run.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
-
-# ...
-# Information about the paper...
-# ...
 #
-# Copyright (C) 20XX ...
-# License: GPLv3
+#
+FREQ0="26.5e9"
+HTX=10
+HRX="1.5"
 
-echo "Figure 1: ..."
-python3 main.py --plot
+NUM_RUNS=250
+
+echo "Figure 2: Distances of destructive interference"
+python example_local_minima.py -v -t "$HTX" -r "$HRX" -dmin 35 -dmax 85 -f 8e9 -df 800e6 -n 3 --plot --export
+
+echo "Table I: Worst-case receive power"
+python assign_frequencies.py -f "$FREQ0" -df 10e6 -n 300 -dmin 45 -dmax 50 -t "$HTX" -r "$HRX" -m 16 --num_runs="$NUM_RUNS" -v --export
+python assign_frequencies.py -f "$FREQ0" -df 10e6 -n 300 -dmin 90 -dmax 100 -t "$HTX" -r "$HRX" -m 16 --num_runs="$NUM_RUNS" -v --export
+python assign_frequencies.py -f "$FREQ0" -df 10e6 -n 300 -dmin 150 -dmax 200 -t "$HTX" -r "$HRX" -m 16 --num_runs="$NUM_RUNS" -v --export
+python assign_frequencies.py -f "$FREQ0" -df 10e6 -n 300 -dmin 150 -dmax 200 -t "$HTX" -r "$HRX" -m 8 --num_runs="$NUM_RUNS" -v --export
+python assign_frequencies.py -f "$FREQ0" -df 10e6 -n 300 -dmin 150 -dmax 200 -t "$HTX" -r "$HRX" -m 32 --num_runs="$NUM_RUNS" -v --export
+python assign_frequencies.py -f "$FREQ0" -df 50e6 -n 60 -dmin 90 -dmax 100 -t "$HTX" -r "$HRX" -m 16 --num_runs="$NUM_RUNS" -v --export
+
+python assign_frequencies.py -f "$FREQ0" -df 1e6 -n 400 -m 8 -dmin 90 -dmax 100 -t "$HTX" -r "$HRX" --num_runs="$NUM_RUNS" -v --export
+python assign_frequencies.py -f "$FREQ0" -df 120e3 -n 3000 -m 8 -dmin 90 -dmax 100 -t "$HTX" -r "$HRX" --num_runs="$NUM_RUNS" -v --export
